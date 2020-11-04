ca05007323dd53c2487aefc401ecca7a658dc811
john <vojohn95@gmail.com>
2020-11-03 19:39:38 -0600

    Add friendly Url

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,10 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name', 'price', 'description', ' thumbnail'];
+
+    protected static function booted()
+    {
+        static::creating(function ($product){
+            $slug = Str::slug($product->name, '-');
+            $product->slug = Product::where('slug', $slug)->exists() ? (slug . uniqid()) : $slug;
+        });
+    }
 }
