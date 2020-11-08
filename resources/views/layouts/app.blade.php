63646f0aedc89dcfa438de26351e7e3f864766d5
john <vojohn95@gmail.com>
2020-11-07 20:33:33 -0600

    Added Logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ url('/') }}"> <img src="{{asset('images/Livecommerce.gif')}}" style="width: 10%;">
                     {{ config('app.name', 'Livewire') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
